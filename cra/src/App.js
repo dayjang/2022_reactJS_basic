53a358a6ee1f6e530d2f00fabd7ab487d8c0d280
dayjang <dianna.jang@gmail.com>
2022-02-22 19:27:47 -0500

    module.css

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,8 +1,9 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 function App() {
   return (
     <div>
-      <h1>Welcome BACK!!!!</h1>
+      <h1 className={styles.title}>Welcome BACK!!!!</h1>
       <Button text="Submit!!" />
     </div>
   );
