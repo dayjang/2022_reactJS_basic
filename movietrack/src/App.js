8218397950d95072871d9785275ab0eb44f4636b
dayjang <dianna.jang@gmail.com>
2022-03-03 09:26:37 -0500

    react-router-dom

--- movietrack/src/App.js
+++ movietrack/src/App.js
@@ -1,20 +1,22 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
   return (
-    <Rounter>
+    <Router>
       <Switch>
+        <Route path="/detail">
+          <h1>detail</h1>
+        </Route>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
         <Route path="/">
           <Home />
         </Route>
-        <Route path="/movie">
-          <Detail />
-        </Route>
       </Switch>
-    </Rounter>
+    </Router>
   );
 }
 
